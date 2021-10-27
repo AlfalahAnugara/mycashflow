84ddd42d6e73a78cc7185730796276474179e9b3
alfalahanugara <alfalah190299@gmail.com>
2021-10-27 09:43:17 +0700

    Membuat Class HomeActivity.java dan activity_home.xml

---
+++ app/src/main/java/org/aplas/buku_kas_nusantara/HomeActivity.java
@@ -0,0 +1,13 @@
+package org.aplas.buku_kas_nusantara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
