007b2a9f47d41f3f2e96b19800fc12bc50ae4b03
alfalahanugara <alfalah190299@gmail.com>
2021-10-27 10:46:21 +0700

    Membuat Deklarasi Object, Menyambungkan element ID yang ada di file xml, Membuat fungsi button menggunakan setOnClickListener yang berfungsi untuk penanganan proses ketika kita mengklik button tersebut

--- app/src/main/java/org/aplas/buku_kas_nusantara/LoginActivity.java
+++ app/src/main/java/org/aplas/buku_kas_nusantara/LoginActivity.java
@@ -1,14 +1,20 @@
 package org.aplas.buku_kas_nusantara;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.aplas.buku_kas_nusantara.Database.DBHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText idUsername, idPassword ;
     Button buttonLogin, buttonRegister ;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,36 @@ public class LoginActivity extends AppCompatActivity {
         idPassword = (EditText) findViewById(R.id.idPassword);
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        DB = new DBHelper(this);
 
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = idUsername.getText().toString();
+                String pass = idPassword.getText().toString();
+
+                if(user.equals("")|pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true) {
+                        Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Username atau Password Salah", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
