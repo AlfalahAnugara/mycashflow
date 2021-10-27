061a395d0cd3c0aa9a9a36533272de8640ff1bfb
alfalahanugara <alfalah190299@gmail.com>
2021-10-27 10:17:29 +0700

    Membuat Deklarasi Object dan menyambungkan element ID yang ada pada file.xml

--- app/src/main/java/org/aplas/buku_kas_nusantara/LoginActivity.java
+++ app/src/main/java/org/aplas/buku_kas_nusantara/LoginActivity.java
@@ -2,12 +2,24 @@ package org.aplas.buku_kas_nusantara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText idUsername, idPassword ;
+    Button buttonLogin, buttonRegister ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        idUsername = (EditText) findViewById(R.id.idUsername);
+        idPassword = (EditText) findViewById(R.id.idPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+
     }
 }
